6960ed79358a3fed2b27fbeda75c087bba6c7567
Oladapo Lawore <dapolawore@gmail.com>
2022-12-15 15:56:43 +0100

    file upload

--- main.py
+++ main.py
@@ -89,30 +89,30 @@ ALLOWED_EXTENSIONS = set(['csv', ])
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @app.route('/file-upload', methods=['POST'])
-# def upload_file():
-# 	# check if the post request has the file part
-# 	if 'file' not in request.files:
-# 		resp = jsonify({'message' : 'No file part in the request'})
-# 		resp.status_code = 400
-# 		return resp
-# 	file = request.files['file']
-# 	if file.filename == '':
-# 		resp = jsonify({'message' : 'No file selected for uploading'})
-# 		resp.status_code = 400
-# 		return resp
-# 	if file and allowed_file(file.filename):
-# 		filename = secure_filename(file.filename)
-# 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#         filename1 = filename
-#         print('filename1  >>', filename1)
-#         resp = jsonify({'message' : 'File successfully uploaded'})
-# 		resp.status_code = 201
-# 		return resp
-# 	else:
-# 		resp = jsonify({'message' : 'Allowed file types is csv'})
-# 		resp.status_code = 400
-# 		return resp
+@app.route('/file-upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message' : 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        filename1 = filename
+        print('filename1  >>', filename1)
+        resp = jsonify({'message' : 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message' : 'Allowed file types is csv'})
+        resp.status_code = 400
+        return resp
 
 
 
